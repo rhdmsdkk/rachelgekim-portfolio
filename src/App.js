63ce488afc83f6fbbb14411d24b33f9eb0bb1ebc
Rachel Kim <rachelgekim@gmail.com>
2024-06-22 00:51:37 -0400

    Setup components & fix file compilation error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Sidebar from './src/components/sidebar/Sidebar';
-import Home from './src/components/home/Home';
-import About from './src/components/about/About';
-import Resume from './src/components/resume/Resume';
-import Portfolio from './src/components/portfolio/Portfolio';
-import Contact from './src/components/contact/Contact';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import "./sidebar.css";
+import Logo from "../../assets/logo.svg";
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <aside className='aside'>
+            <a href="#home" className="nav__logo">
+                <img src={Logo} alt="" />
+            </a>
+
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link">
+                                <i className="icon-home"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </aside>
     )
 }
 
